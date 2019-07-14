019e9c17cac45b931a9df312f9eea41088505876
AscendoHack <52598832+AscendoHack@users.noreply.github.com>
2019-07-14 12:13:37 +0700

    Add files via upload

---
+++ tool.sh
@@ -0,0 +1,538 @@
+#!/bin/bash
+clear
+#Malu Lah Anjeng Ngerecode Mulu
+#Hargai Lah Sang pembuat
+#Tak Punya Otak Kau Ya
+#Awas Kalau Recode
+clear
+figlet aScEnDoID TuToRiAl | lolcat
+sleep 5
+
+echo "[+]==================================[+]" | lolcat
+echo "[+] •Code By   : TU4N3NTUP           [+]" | lolcat
+echo "[+] •Youtube   : AscendoID Tutorial  [+]" | lolcat
+echo "[+] •Contact   : Ascendo ID Facebook [+]" | lolcat
+echo "[+] •Team      : KemprutTeamHacker   [+]" | lolcat
+echo "[+] •Instagram : Ascendo ID          [+]" | lolcat
+echo "[+] •Version   : 1.6                 [+]" | lolcat
+echo "[+]==================================[+]" | lolcat
+echo ""
+echo "Jika Ada Masalah Mohon Hubungi Fb : Ascendo ID" | lolcat
+echo "Jangan Lupa Subscribe Chanell AscendoID Tutorial Ya" | lolcat
+echo "Jika Ada Tools Lain Lagi Yg Mau Di Tambah Hubungi Admin.." | lolcat
+echo ""
+echo ""
+echo
+echo "================================"
+echo "[√]MENU[√]" | lolcat
+echo "================================"
+echo "[1]Tombol Kanan Kiri Termux....." | lolcat
+echo "================================"
+echo "[2]Dark Facebook..." | lolcat
+echo "================================"
+echo "[3]Microsploit...." | lolcat
+echo "================================"
+echo "[4]Anti Root....." | lolcat
+echo "================================"
+echo "[5]CrawL...." | lolcat
+echo "================================"
+echo "[6]Dark Facebook V2...." | lolcat
+echo "================================"
+echo "[7]Terminals...." | lolcat
+echo "================================"
+echo "[8]Unmarshal Python..." | lolcat
+echo "================================"
+echo "[9]Bruntal Sms...." | lolcat
+echo "================================"
+echo "[10]Instagram Auto Follow..." | lolcat
+echo "================================"
+echo "[11]MBF Facebook...." | lolcat
+echo "================================"
+echo "[12]Belajar Bahasa Python.." | lolcat
+echo "================================"
+echo "[13]Belajar C++..." | lolcat
+echo "================================"
+echo "[14]Belajar Javascript..." | lolcat
+echo "================================"
+echo "[15]Belajar Bahasa Php.." | lolcat
+echo "================================"
+echo "[16]Belajar Dasar Dasar Termux." | lolcat
+echo "================================"
+echo "[17]Hammer.." | lolcat
+echo "================================"
+echo "[18]LiteSpam..." | lolcat
+echo "================================"
+echo "[19]Bajingan V6...." | lolcat
+echo "================================"
+echo "[20]Metasploit.." | lolcat
+echo "================================"
+echo "[21]Anti Ddos.." | lolcat
+echo "================================"
+echo "[22]Termux Ohmyzsh.." | lolcat
+echo "================================"
+echo "[23]Youtube Download.." | lolcat
+echo "================================"
+echo "[24]mp3 Download..." | lolcat
+echo "================================"
+echo "[25]Membuat Virus.." | lolcat
+echo "================================"
+echo "[26]Termux Banner..." | lolcat
+echo "================================"
+echo "[27]Fb Spam..." | lolcat
+echo "================================"
+echo "[28]Kk Dan Ktp Gratis.." | lolcat
+echo "================================"
+echo "[29]Virtex.." | lolcat
+echo "================================"
+echo "[30]Spam Whatsapp.." | lolcat
+echo "================================"
+echo "[31]Sadap Handphone..." | lolcat
+echo "================================"
+echo "[32]Yahoo Cloning..." | lolcat
+echo "================================"
+echo "[00]Exit.." | lolcat
+echo "================================"
+read -p "Masukan pilihan : " cok;
+
+if [ $cok = 1 ] || [ $cok = 1 ]
+then
+clear
+figlet Keyboard | lolcat
+sleep 3
+echo "Mohon Tunggu..."
+pkg install git
+pkg install python2
+git clone https://github.com/AscendoHack/ascendo12
+cd ascendo12
+python2 run.py
+fi
+
+if [ $cok = 2 ] || [ $cok = 2 ]
+then
+clear
+figlet Dark Fb Kntl | lolcat
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+fi
+
+if [ $cok = 3 ] || [  $cok = 3 ]
+then
+clear
+figlet Microsploit | lolcat
+sleep 3
+echo "Menginstall Bahan..." | lolcat
+pkg update && pkg upgrade
+pkg install git
+pkg install bash
+pip install requests
+pip install mechanize
+git clone https://github.com/pashayogi/Microsploit
+cd Microsploit
+chmod 777 Microsploit
+echo "Jalankan Dengan Perintah" | lolcat
+echo "./Microsploit" | lolcat
+fi
+
+if [ $cok = 4 ] || [ $cok = 4 ]
+then
+clear
+figlet Anti Root | lolcat
+sleep 3
+
+echo " Program Ini Menjalankan Root Tanpa Root" | lolcat
+echo ""
+pkg update && pkg upgrade
+pkg install unzip
+git clone https://github.com/pashayogi/anti-ROOT
+cd anti-ROOT
+pkg install -y Fakerootall.deb
+echo " JalanKan Dengan Perintah Fakeroot Lalu Habis Itu whoami"
+echo "SUKSES" | lolcat
+sleep 2
+exit
+fi
+
+if [ $cok = 5 ] || [ $cok = 5 ]
+then
+clear
+figlet Crawl | lolcat
+pkg install git
+pkg install unzip
+pkg install python2
+pip2 install requests
+git clone https://github.com/pashayogi/CrawL
+cd CrawL
+unzip CrawL.zip
+python2 dCrawL.py
+fi
+
+if [ $cok = 6 ] || [ $cok = 6 ]
+then
+clear
+figlet Fb Kntl V2 | lolcat
+sleep 3
+echo "Menginstall Bahan.." | lolcat
+pkg install git
+pkg inatall python2
+pip install requests
+pip install mechanize
+git clone https://github.com/pashayogi/SETAN2
+cd SETAN2
+python2 SETAN2.py
+fi
+
+if [ $cok = 7 ] || [ $cok = 7 ]
+then
+clear
+figlet TERMINAL XXX | lolcat
+sleep 4
+echo "Menginstall Bahan...." | lolcat
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+pip2 install requests
+pkg install lolcat
+git clone https://github.com/pashayogi/TERMINALS
+cd TERMINALS
+sh su.sh
+fi
+
+if [ $cok = 8 ] || [ $cok = 8 ]
+then
+clear
+figlet Unmarshal Pyth | lolcat
+pkg update && pkg upgrade
+pkg install git
+pkg install python
+git clone https://github.com/AhmadRiswanto704/comarsh
+cd comarsh
+python2 comarsh.py
+fi
+
+if [ $cok = 9 ] || [ $cok = 9 ]
+then
+clear
+pkg update && pkg upgrade
+pkg install git
+pkg install python python2
+git Clone https://github.com/TERMUXID3/brutal-sms
+cd bruntal-sms
+python2 run.py
+fi
+
+if [ $cok = 10 ] || [ $cok = 10]
+then
+clear
+figlet Instagram | lolcat
+sleep 4
+pkg install git
+git clone https://github.com/AscendoHack/instagram
+cd instagram
+sh instagram.sh
+fi
+
+if [ $cok = 11 ] || [ $cok = 11 ]
+then
+clear
+figlet MBF kntl | lolcat
+sleep 3
+echo "Menginstall Bahan..." | lolcat
+pkg install python2 git
+pip2 install mechanize
+git clone https://github.com/Senitopeng/fbbrute
+cd fbbrute
+python2 jomblo.py
+fi
+
+if [ $cok = 12 ] || [ $cok = 12 ]
+then
+clear
+figlet Bahasa | lolcat
+sleep 3
+echo " Menginstall Bahan..." | lolcat
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/AscendoHack/bahasa
+cd bahasa
+python2 bahasa.py
+fi
+
+if [ $cok = 13 ] || [ $cok = 13 ]
+then
+clear
+figlet C++ | lolcat
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/AscendoHack/bahasac
+cd bahasa
+python2 c++.py
+fi
+
+if [ $cok = 14 ] || [ $cok = 14 ]
+then
+clear
+figlet Javascript | lolcat
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/AscendoHack/bahasa
+cd bahasa
+python2 java.py
+fi
+
+if [ $cok = 15 ] || [ $cok = 15 ]
+then
+clear
+figlet PHP | lolcat
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/AscendoHack/bahasa
+cd bahasa
+python2 php.py
+fi
+
+if [ $cok = 16 ] || [ $cok = 16 ]
+then
+clear
+figlet TERMUX | lolcat
+echo "Menginstall Bahan Dulu..."
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/AscendoHack/bahasa
+cd bahasa
+python2 termux.py
+fi
+
+if [ $cok = 17 ] || [ $cok = 17 ]
+then
+clear
+figlet Hammer | lolcat
+pkg update
+pkg upgrade
+pkg install python
+pkg install git
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+fi
+
+if [ $cok = 18 ] || [ $cok = 18 ]
+then
+clear
+figlet LiteSpam | lolcat
+apt update && apt upgrade
+apt install php
+apt install python2
+apt install toilet
+apt install git
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $cok = 19 ] || [ $cok = 19 ]
+then
+clear
+figlet B4JINGAN V6 | lolcat
+apt update apt upgrade
+apt install git
+apt install php
+apt install figlet
+apt install toilet
+apt install python2
+Install Bahan Pip2
+pip2 install requests
+pip2 install termcolor
+Cloning Script Nya
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6.git
+Buka Folder
+cd BAJINGANv6
+echo "Masukkan Username Ini" | lolcat
+echo "username : BAJINGAN"
+echo "Masukkan Password Ini" | lolcat
+echo "password : Gans"
+sleep 6
+sh BAJINGAN.sh
+fi
+
+if [ $cok = 20 ] || [ $cok = 20 ]
+then
+clear
+figlet Metasploit | lolcat
+echo " Mohon Tunggu..."
+pkg install unstable-repo
+pkg install metasploit
+msfconsole
+fi
+
+if [ $cok = 21 ] || [ $cok = 21 ]
+then
+clear
+figlet Anti DDOS | lolcat
+pkg install git
+git clone https://github.com/ismailtasdelen/Anti-DDOS
+cd Anti-DDOS
+sh anti-ddos.sh
+fi
+
+if [ $cok = 22 ] || [ $cok = 22 ]
+then
+clear
+figlet Cmatrix | lolcat
+apt update && apt upgrade
+apt install nano
+apt install cmatrix
+apt install ruby
+gem install lolcat
+apt install figlet
+apt install neofetch
+apt install toilet
+git clone https://github.com/Cabbagec/termux-ohmyzsh
+cd termux-ohmyzsh
+chmod +x install.sh
+sh ./install.sh
+fi
+
+if [ $cok = 23 ] || [ $cok = 23 ]
+then
+figlet Youtube | lolcat
+pkg install git
+pkg install python2
+git clone https://github.com/AscendoHack/Download
+cd Download
+python2 yt.py
+fi
+
+if [ $cok = 24 ] || [ $cok = 24 ]
+then
+figlet Mp3 Download | lolcat
+pkg install git
+pkg install python2
+git clone https://github.com/AscendoHack/Download
+cd Download
+python2 mp3.py
+fi
+
+if [ $cok = 25 ] || [ $cok = 25 ]
+then
+clear
+figlet Virus | lolcat
+apt update && apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/Junior60/vbug
+cd vbug
+python2 vbug.py
+fi
+
+if [ $cok = 26 ] || [ $cok = 26 ]
+then
+clear
+figlet TU4N3NTUP | lolcat
+git clone https://github.com/Bhai4You/Termux-Banner
+cd Termux-Banner
+chmod +x requirement.sh
+chmod +x t-ban.sh
+bash requirement.sh
+bash t-ban.sh
+fi
+
+if [ $cok = 27 ] || [ $cok = 27 ]
+then
+clear
+figlet Fb Spam | lolcat
+pkg install unzip
+pkg install python2
+pkg install git
+git clone https://github.com/pashayogi/Fbspam
+cd Fbspam
+python2 fbspam.py
+fi
+
+if [ $cok = 28 ] || [ $cok = 28 ]
+then
+clear
+figlet Nik And Kk
+apt install git php php-cli php-curl php-dev
+git clone https://github.com/ghostrecoded/kkktpgenerate2/
+cd kkktpgenerate2
+php gogenerate.php
+fi
+
+if [ $cok = 29 ] || [ $cok = 29 ]
+then
+clear
+figlet Virtex | lolcat
+pkg update && pkg upgrade
+pkg install git
+pkg install curl
+pkg install figlet
+pkg install ruby
+pkg install lolcat
+git clone https://github.com/muhammadfathul/VIRTEX
+cd VIRTEX
+chmod 777 virtex.sh
+sh virtex.sh
+fi
+
+if [ $cok = 30 ] || [ $cok = 30 ]
+then
+clear
+figlet Spam Whatsapp | lolcat
+sleep 3
+echo "[√]------------------[√]" | lolcat
+echo "[√]    WARNING!!!    [√]" | lolcat
+echo "[√]------------------[√]" | lolcat
+echo "[√]Work Atau Tidaknya[√]" | lolcat
+echo "[√]Tergantung Anda Se[√]" | lolcat
+echo "[√]ndiri Menggunakan [√]" | lolcat
+echo "[√]Nya Dalam Kebaikan[√]" | lolcat
+echo "[√]Atau Kejelekan... [√]" | lolcat
+echo "[√]  ttd : Entup     [√]" | lolcat
+echo "[√]------------------[√]" | lolcat
+sleep 5
+apt update && apt upgrade
+pkg install git
+pkg install php
+git clone https://github.com/siputra12/prank
+cd prank
+php wa.php
+fi
+
+if [ $cok = 31 ] || [ $cok = 31 ]
+then
+clear
+figlet Sadap Hp | lolcat
+sleeo 3
+echo "Mohon Gunakan Dengan Kebaikan." | lolcat
+echo "Anda Harus Sudah Install Metasploit.." | lolcat
+apt update && apt upgrade
+apt install git
+git clone https://github.com/4L13199/meTAInstall
+chmod +x meTAInstall
+sh meTAInstall
+fi
+
+if [ $cok = 32 ] || [ $cok = 32 ]
+then
+figlet Yahoo Cloning | lolcat
+pkg update && pkg upgrade
+pkg install python2 git
+pip2 install requests mechanize
+git clone https://github.com/wahyuandhika/YahooCloning
+cd YahooCloning
+python cloning.py
+fi
+
+if [ $cok = 00 ] || [ $cok = 00 ]
+then
+figlet Exit | lolcat
+fi
